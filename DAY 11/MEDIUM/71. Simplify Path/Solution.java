c171ef7839d4a68fac2390a980e2a277a04d4da3
ShivamRay3105 <shivamray3105@gmail.com>
2026-07-16 00:56:55 +0530

    Skipped for now

---
+++ DAY 11/MEDIUM/71. Simplify Path/Solution.java
@@ -0,0 +1,16 @@
+// public Solution {
+
+// }
+
+/*
+ * notes for stack:
+ * 
+ * if stack is like
+ * 
+ * World
+ * Hello
+ * 
+ * and we need want to convert the components into string or something
+ * in which we want the first inserted element,
+ * we can use for each loop and iterate through the stack
+ */
