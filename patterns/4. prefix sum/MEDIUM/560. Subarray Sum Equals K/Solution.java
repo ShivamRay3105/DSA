19507df067987e9db270fae32ef2d5686be9ae23
ShivamRay3105 <shivamray3105@gmail.com>
2026-07-21 23:17:37 +0530

    hashmap + prefixsum, QUES 560 done

---
+++ patterns/4. prefix sum/MEDIUM/560. Subarray Sum Equals K/Solution.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum[] = new int[nums.length];
+        prefixSum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefixSum[i] = nums[i] + prefixSum[i - 1];
+        }
+        map.put(0, 1);
+
+        int arrayCounter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(prefixSum[i] - k)) {
+                arrayCounter += map.get(prefixSum[i] - k);
+            }
+            map.put(prefixSum[i], map.getOrDefault(prefixSum[i], 0) + 1);
+        }
+        return arrayCounter;
+    }
+}
